f778aeba73f4c32eba71f9273d513a118a9497ea
Apoorv Verma <40027838+Sunnyverma81201@users.noreply.github.com>
2021-06-04 01:28:29 +0530

    finalized the navigation UI and UX and added about and contact page

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,22 @@
 var hamburger = document.getElementsByClassName("hamburger")[0];
-var navbar =document.getElementsByTagName
+var navbar = document.getElementById("top-bar");
+var topBarElements = document.getElementsByClassName("top-bar-element");
+
+
 hamburger.addEventListener('click',()=>{
+    var i;
     if(hamburger.classList.contains("open")){
         hamburger.classList.remove("open");
+        navbar.classList.remove("open-nav");
+        for(i = 0;i<topBarElements.length; i++){
+            topBarElements[i].classList.remove("on-open-show-items");
+        }
     }
     else{
         hamburger.classList.add("open");
+        navbar.classList.add("open-nav");
+        for(i = 0;i<topBarElements.length; i++){
+            topBarElements[i].classList.add("on-open-show-items");
+        }
     }
 });
